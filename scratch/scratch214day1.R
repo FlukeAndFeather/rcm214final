689ec002e53d5b1d8c58c7e2f9abea1401e29c11
RachelMGitH <rachelcmiller01@gmail.com>
2026-08-27 14:01:40 -0700

    Checkpoint

--- scratch/scratch214day1.R
+++ scratch/scratch214day1.R
@@ -8,48 +8,47 @@ bq1_data <- read_csv("data/knb-lter-luq.20.4923064/QuebradaCuenca1-Bisley.csv")
 bq2_data <- read_csv("data/knb-lter-luq.20.4923064/QuebradaCuenca2-Bisley.csv")
 bq3_data <- read_csv("data/knb-lter-luq.20.4923064/QuebradaCuenca3-Bisley.csv")
 
-# Extracting only the relevant columns to the figure
-clean_bq1 <- bq1_data |> 
-  select(c(Sample_ID, Sample_Date, K, `NO3-N`, Mg, Ca, `NH4-N`))
-
-clean_bq2 <- bq2_data |> 
-  select(c(Sample_ID, Sample_Date, K, `NO3-N`, Mg, Ca, `NH4-N`))
-
-clean_bq3 <- bq3_data |> 
-  select(c(Sample_ID, Sample_Date, K, `NO3-N`, Mg, Ca, `NH4-N`))
-
-clean_prm <- prm_data |> 
-  select(c(Sample_ID, Sample_Date, K, `NO3-N`, Mg, Ca, `NH4-N`))
-
-#Checking number of NAs
-print(sum(is.na(bq1_data$`NO3-N`)))
-
 # Filtering site data
-filtered_bq1 <- clean_bq1 |> 
-  filter(year(Sample_Date) %in% c(1986:1995)) 
-filtered_bq2 <- clean_bq2 |> 
-  filter(year(Sample_Date) %in% c(1986:1995)) 
+filtered_bq1 <- bq1_data |> 
+  filter(year(Sample_Date) %in% c(1988:1995)) 
+filtered_bq2 <- bq2_data |> 
+  filter(year(Sample_Date) %in% c(1988:1995))
+filtered_bq3 <- bq3_data |> 
+  filter(year(Sample_Date) %in% c(1988:1995))
+filtered_prm <- prm_data |> 
+  filter(year(Sample_Date) %in% c(1988:1995))
 
 #moving average function
-moving_average(filtered_data)
+bq1_smoothed <- moving_average(filtered_bq1)
+bq2_smoothed <- moving_average(filtered_bq2)
+bq3_smoothed <- moving_average(filtered_bq3)
+prm_smoothed <- moving_average(filtered_prm)
+
+#Combining into one large tibble
+combined_data <- rbind(
+  bq1_smoothed |> mutate(site = as.factor("BQ1")),
+  bq2_smoothed |> mutate(site = as.factor("BQ2")),
+  bq3_smoothed |> mutate(site = as.factor("BQ3")),
+  prm_smoothed |> mutate(site = as.factor("PRM")) 
+)
+
 
 #Pivot longer
-longer_average <- moving_average(filtered_data =) |> pivot_longer(
-    cols = c(K_mgl, `NO3-N_ugl`, Mg_mgl, Ca_mgl, `NH4-N_ugl`), 
+longer_combined <- pivot_longer(data = combined_data,
+    cols = c(k_mgl, no3_ugl, mg_mgl, ca_mgl, nh4_ugl), 
     values_to = "Concentration",
     names_to = "Ion"
   ) 
 
-ggplot(longer_average,
+ggplot(longer_combined,
     mapping = aes(
       x = window_start, 
       y = Concentration,
-      shape = Ion,
-      colour = Ion
+      linetype = "site"
     )
   ) + geom_line() +
-  facet_wrap("Ion", scales = "free") +
-  labs(title = "BQ2")
+  facet_wrap("Ion", scales = "free", ncol = 1) +
+  labs(title = "Figure 3")
 
 
 # Using combined data frame
@@ -77,3 +76,16 @@ filtered_smoothed <- filtered_combined_data |>
     )
   ) + geom_line()
 print(filtered_smoothed)
+
+
+clean_bq1 <- bq1_data |> 
+  select(c(Sample_ID, Sample_Date, K, `NO3-N`, Mg, Ca, `NH4-N`))
+
+clean_bq2 <- bq2_data |> 
+  select(c(Sample_ID, Sample_Date, K, `NO3-N`, Mg, Ca, `NH4-N`))
+
+clean_bq3 <- bq3_data |> 
+  select(c(Sample_ID, Sample_Date, K, `NO3-N`, Mg, Ca, `NH4-N`))
+
+clean_prm <- prm_data |> 
+  select(c(Sample_ID, Sample_Date, K, `NO3-N`, Mg, Ca, `NH4-N`))
