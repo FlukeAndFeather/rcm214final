c15f688619d29d53c55d706da14179799bf05554
RachelMGitH <rachelcmiller01@gmail.com>
2026-08-28 01:20:03 -0700

    committing edits

--- R/moving-average.R
+++ R/moving-average.R
@@ -4,23 +4,21 @@ moving_average <- function(filtered_data) {
   # Initialize a tibble to contain the results
   result <- tibble(
     window_start = seq(ymd(filtered_data$Sample_Date[1]), ymd(filtered_data$Sample_Date[nrow(filtered_data)]), by = "9 weeks"),
-    site = filtered_data$Sample_ID[1], # This line will create a site column in your tibble, which will be populated accordingly by each of the filtered dataframes when you run the for loop
+    Site = filtered_data$Sample_ID[1], # This line will create a site column in your tibble, which will be populated accordingly by each of the filtered dataframes when you run the for loop
     k_mgl = NA,
     mg_mgl = NA,
   no3_ugl = NA,
   ca_mgl = NA,
   nh4_ugl= NA
-    # Fill in the rest of the ions
   )
 
-  
   # Fill in the iterator and sequence
   for (i in 1:nrow(result)) {
     # Create variables for the start and end of the current window
     w1 <- result$window_start[i]
     w2 <- w1 + weeks(9)
     # Create a logical vector, called "in_window", that says which samples are inside the window
-    # Hint: you'll compare sample dates to the start and end of the window
+
     in_window <- filtered_data$Sample_Date >= w1 &
       filtered_data$Sample_Date < w2
 
@@ -30,17 +28,13 @@ moving_average <- function(filtered_data) {
     no3_window <- filtered_data$`NO3-N`[in_window]
     ca_window <- filtered_data$Ca[in_window]
     nh4_window <- filtered_data$`NH4-N`[in_window]
-    
-    # The line above gets potassium in the window. Get the rest of the ions too
 
     # Calculate the mean of each ion concentration and fill in the result
-    result$k_mgl[i] <- mean(k_window)
-    result$mg_mgl[i] <- mean(mg_window)
-    result$no3_ugl[i] <- mean(no3_window)
-    result$ca_mgl[i] <- mean(ca_window)
-    result$nh4_ugl[i] <- mean(nh4_window)
+    result$k_mgl[i] <- mean(k_window, na.rm = TRUE)
+    result$mg_mgl[i] <- mean(mg_window, na.rm = TRUE)
+    result$no3_ugl[i] <- mean(no3_window, na.rm = TRUE)
+    result$ca_mgl[i] <- mean(ca_window, na.rm = TRUE)
+    result$nh4_ugl[i] <- mean(nh4_window, na.rm = TRUE)
   }
-  
   return(result)
-  # Return the result
 }
--- scratch/scratch214day1.R
+++ scratch/scratch214day1.R
@@ -1,63 +1,63 @@
 library(tidyverse)
 source("R/moving-average.R")
 
-# Goal: Figure with each Ion represented separately (facet wrap) for years 1988 to 1995
-# Reading in all the data
+# Reading in all the raw data
 prm_data <- read_csv("data/knb-lter-luq.20.4923064/RioMameyesPuenteRoto.csv")
 bq1_data <- read_csv("data/knb-lter-luq.20.4923064/QuebradaCuenca1-Bisley.csv")
 bq2_data <- read_csv("data/knb-lter-luq.20.4923064/QuebradaCuenca2-Bisley.csv")
 bq3_data <- read_csv("data/knb-lter-luq.20.4923064/QuebradaCuenca3-Bisley.csv")
 
-# Filtering site data
+# Filtering site data for each site to the relevant time frame
+filtered_prm <- prm_data |> 
+  filter(year(Sample_Date) %in% c(1988:1995))
 filtered_bq1 <- bq1_data |> 
   filter(year(Sample_Date) %in% c(1988:1995)) 
 filtered_bq2 <- bq2_data |> 
   filter(year(Sample_Date) %in% c(1988:1995))
 filtered_bq3 <- bq3_data |> 
   filter(year(Sample_Date) %in% c(1988:1995))
-filtered_prm <- prm_data |> 
-  filter(year(Sample_Date) %in% c(1988:1995))
 
-#moving average function
+# Calling the stored moving average function to calculate moving average for each site
+prm_smoothed <- moving_average(filtered_prm)
 bq1_smoothed <- moving_average(filtered_bq1)
 bq2_smoothed <- moving_average(filtered_bq2)
 bq3_smoothed <- moving_average(filtered_bq3)
-prm_smoothed <- moving_average(filtered_prm)
 
-#Combining into one large tibble
+#Combining the results from moving average into one tibble including the site names
 combined_data <- rbind(
-  bq1_smoothed |> mutate(site = as.factor("BQ1")),
-  bq2_smoothed |> mutate(site = as.factor("BQ2")),
-  bq3_smoothed |> mutate(site = as.factor("BQ3")),
-  prm_smoothed |> mutate(site = as.factor("PRM")) 
+  prm_smoothed |> mutate(Site = as.factor("PRM")),
+  bq1_smoothed |> mutate(Site = as.factor("BQ1")),
+  bq2_smoothed |> mutate(Site = as.factor("BQ2")),
+  bq3_smoothed |> mutate(Site = as.factor("BQ3")) 
 )
 
-
-#Pivot longer
-longer_combined <- pivot_longer(data = combined_data,
+#Pivot longer to prepare the data frame for plotting
+longer_combined <- combined_data |> 
+  pivot_longer(
     cols = c(k_mgl, no3_ugl, mg_mgl, ca_mgl, nh4_ugl), 
     values_to = "Concentration",
     names_to = "Ion"
   ) 
 
+# Plotting the data
 ggplot(longer_combined,
     mapping = aes(
       x = window_start, 
       y = Concentration,
-      linetype = "site"
+      linetype = Site
     )
   ) + geom_line() +
-  facet_wrap("Ion", scales = "free", ncol = 1) +
-  labs(title = "Figure 3")
+  facet_wrap("Ion", scales = "free", ncol = 1, strip.position = "left") +
+  labs(title = "Concentrations in Bisley, Puerto Rico Streams before and after Hurricane Hugo") +
+  xlab("Years")
 
 
-# Using combined data frame
-# Combining into one data frame (not running right now)
+# Safety blanket code
+
+# Combining into one data frame
 combined_data <- rbind(clean_bq1, clean_bq2, clean_bq3, clean_prm)
 filtered_combined_data <- combined_data |> 
   filter(year(Sample_Date) %in% c(1988:1995))
-
-
 
 
 # Plotting the concentration over years (moving average not calculated yet) - Day 1 Activity
@@ -77,15 +77,3 @@ filtered_smoothed <- filtered_combined_data |>
   ) + geom_line()
 print(filtered_smoothed)
 
-
-clean_bq1 <- bq1_data |> 
-  select(c(Sample_ID, Sample_Date, K, `NO3-N`, Mg, Ca, `NH4-N`))
-
-clean_bq2 <- bq2_data |> 
-  select(c(Sample_ID, Sample_Date, K, `NO3-N`, Mg, Ca, `NH4-N`))
-
-clean_bq3 <- bq3_data |> 
-  select(c(Sample_ID, Sample_Date, K, `NO3-N`, Mg, Ca, `NH4-N`))
-
-clean_prm <- prm_data |> 
-  select(c(Sample_ID, Sample_Date, K, `NO3-N`, Mg, Ca, `NH4-N`))
